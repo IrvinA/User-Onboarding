0598f5ab093b1d2299952b6a91de7d3c9f62a788
IrvinA <arevalosirvin@gmail.com>
2021-09-15 15:30:24 -0700

    Assignment and styling complete

--- user-onboarding/src/User.js
+++ user-onboarding/src/User.js
@@ -7,18 +7,39 @@ const StyledH3 = styled.h3`
     color: red;
 `
 
+const StyledDiv = styled.div`
+    text-align: center;
+    margin-bottom: 5%;
+    h2 {
+        font-size: 2rem;
+        border-bottom: 1px solid black;
+        width: 30%;
+        margin-left: 35%;
+        text-shadow: 1px 1px lime;
+    }
+    p {
+        background-color: lightgray;
+        color: lime;
+        font-weight: 600;
+        text-shadow: 1px 1px black;
+        margin: 0 35%;
+        padding: 2% 0;
+        font-size: 1.5rem;
+    }
+`
+
 function User({ details }) {
     if (!details) {
         return <StyledH3>Loading User Details</StyledH3>
     }
 
     return (
-        <div>
+        <StyledDiv>
             <h2>{details.name}</h2>
             <p>Age: {details.age}</p>
             <p>Email: {details.email}</p>
             <p>Password: {details.password}</p>
-        </div>
+        </StyledDiv>
     )
 }
 
